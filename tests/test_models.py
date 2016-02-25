8db61bec4c9b2bf5597cf87db8ad1876f21a1abb
cjmabry <cjmab28@gmail.com>
2016-02-25 14:46:43 -0600

    Got rid of uneeded stuff

--- tests/test_models.py
+++ tests/test_models.py
@@ -3,11 +3,3 @@
 from fbone.user import User, UserDetail
 
 from tests import TestCase
-
-
-class TestUser(TestCase):
-
-    def test_get_current_time(self):
-
-        assert User.query.count() == 2
-        assert UserDetail.query.count() == 2
